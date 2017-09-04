203a73c8d2c66a560009dbf02d16bdb95186830d
Aymen Mehri <aymen.mehri@soat.fr>
2017-09-04 14:48:27 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'aym-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'aym';
+}
