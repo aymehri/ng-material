cab76dfb66acb539bf147b310639243b20a720e0
Aymen Mehri <aymen.mehri@soat.fr>
2017-09-04 15:31:03 +0200

    add all material module

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,78 @@
 import { NgModule } from '@angular/core';
-import { MdButtonModule, MdCheckboxModule } from '@angular/material';
+import {
+  // Forms Controls
+  MdAutocompleteModule,
+  MdCheckboxModule,
+  MdDatepickerModule,
+  MdInputModule,
+  MdRadioModule,
+  MdSelectModule,
+  MdSliderModule,
+  MdSlideToggleModule,
+  // Navigation
+  MdMenuModule,
+  MdSidenavModule,
+  MdToolbarModule,
+  // Layout
+  MdListModule,
+  MdGridListModule,
+  MdCardModule,
+  MdTabsModule,
+  // Buttons & indicators
+  MdButtonModule,
+  MdButtonToggleModule,
+  MdChipsModule,
+  MdIconModule,
+  MdProgressSpinnerModule,
+  MdProgressBarModule,
+  // Popups & Modals
+  MdDialogModule,
+  MdTooltipModule,
+  MdSnackBarModule,
+  // Data Table
+  MdTableModule,
+  MdSortModule,
+  MdPaginatorModule
+} from '@angular/material';
+
+const MATERIALMODULE = [
+  // Forms Controls
+  MdAutocompleteModule,
+  MdCheckboxModule,
+  MdDatepickerModule,
+  MdInputModule,
+  MdRadioModule,
+  MdSelectModule,
+  MdSliderModule,
+  MdSlideToggleModule,
+  // Navigation
+  MdMenuModule,
+  MdSidenavModule,
+  MdToolbarModule,
+  // Layout
+  MdListModule,
+  MdGridListModule,
+  MdCardModule,
+  MdTabsModule,
+  // Buttons & indicators
+  MdButtonModule,
+  MdButtonToggleModule,
+  MdChipsModule,
+  MdIconModule,
+  MdProgressSpinnerModule,
+  MdProgressBarModule,
+  // Popups & Modals
+  MdDialogModule,
+  MdTooltipModule,
+  MdSnackBarModule,
+  // Data Table
+  MdTableModule,
+  MdSortModule,
+  MdPaginatorModule
+];
 
 @NgModule({
-  imports: [MdButtonModule, MdCheckboxModule],
-  exports: [MdButtonModule, MdCheckboxModule],
+  imports: [...MATERIALMODULE],
+  exports: [...MATERIALMODULE],
 })
 export class MyOwnCustomMaterialModule { }
